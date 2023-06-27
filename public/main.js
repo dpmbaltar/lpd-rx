0a25887d5a76013890d470fe05498fd50dbd7a5e
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2023-06-27 12:24:01 -0300

    Usar clases de bootstrap

--- public/main.js
+++ public/main.js
@@ -3,15 +3,17 @@ const url = "ws://localhost:8080";
 
 let connected = false;
 let socket;
-let statusBar;
+let statusText;
 let contentBox;
+let nameInput;
 let textInput;
 let sendButton;
 let openCloseButton;
 
 function connect() {
+  nameInput.setAttribute("disabled", true);
   openCloseButton.setAttribute("disabled", true);
-  statusBar.textContent = "Conectando...";
+  statusText.textContent = "Conectando...";
 
   // Create socket
   socket = new WebSocket(url, "echo-protocol");
@@ -19,35 +21,44 @@ function connect() {
   // Socket open event handler
   socket.addEventListener("open", function() {
     connected = true;
-    statusBar.textContent = "Conectado";
+    statusText.textContent = "Conectado";
     textInput.removeAttribute("disabled");
     sendButton.removeAttribute("disabled");
     openCloseButton.removeAttribute("disabled");
-    openCloseButton.setAttribute("value", "Desconectarse");
+    openCloseButton.className = "btn btn-danger";
+    openCloseButton.textContent = "Desconectarse";
   });
 
   // Socket close event handler
   socket.addEventListener("close", function() {
     connected = false;
-    statusBar.textContent = "Desconectado";
+    statusText.textContent = "Desconectado";
     textInput.value = "";
     textInput.setAttribute("disabled", true);
     sendButton.setAttribute("disabled", true);
-    openCloseButton.setAttribute("value", "Conectarse");
     openCloseButton.removeAttribute("disabled");
+    openCloseButton.className = "btn btn-success";
+    openCloseButton.textContent = "Conectarse";
+    nameInput.removeAttribute("disabled");
   });
 
   // Socket message event handler
   socket.addEventListener("message", function(messageEvent) {
-    const newMessage = document.createElement("div");
-    newMessage.textContent = messageEvent.data;
-    contentBox.appendChild(newMessage);
+    const message = JSON.parse(messageEvent.data);
+    const messageCard = document.createElement("div");
+    const messageBody = document.createElement("div");
+
+    messageBody.textContent = message.usr + ' dice: ' + message.txt;
+    messageBody.className = "card-body";
+    messageCard.className = "card mb-2";
+    messageCard.appendChild(messageBody);
+    contentBox.appendChild(messageCard);
     contentBox.scrollTop = contentBox.scrollHeight;
   });
 
   // Socket error event handler
   socket.addEventListener("error", function(event) {
-    statusBar.textContent = "Error: " + event;
+    statusText.textContent = "Error: " + event;
   });
 }
 
@@ -56,16 +67,19 @@ function disconnect() {
 }
 
 function send() {
-  const message = textInput.value;
+  const name = nameInput.value;
+  const text = textInput.value;
+  const message = { usr: name, txt: text };
   textInput.value = "";
-  socket.send(message);
+  socket.send(JSON.stringify(message));
 }
 
 window.addEventListener("load", function() {
 
   // Get HTML elements
-  statusBar = this.document.getElementById("chat-status-bar");
+  statusText = this.document.getElementById("chat-status-text");
   contentBox = this.document.getElementById("chat-content-box");
+  nameInput = this.document.getElementById("chat-input-name");
   textInput = this.document.getElementById("chat-input-text");
   sendButton = this.document.getElementById("chat-input-send");
   openCloseButton = this.document.getElementById("chat-input-open-close");
